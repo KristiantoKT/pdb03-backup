9e9e7ec2722945f6e38e302867693c84b314df9e
Kristianto <liauwkristianto@gmail.com>
2019-11-28 20:46:16 +0700

    Fix docker-compose to managed CORS

--- src/App.js
+++ src/App.js
@@ -3,25 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-function elasticsearch() {
-  const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
-  const URL = 'http://localhost:9200/';
-  // const response = await axios.get('http://localhost:9200/',{
-  //   mode: 'no-cors',
-  //   headers: {
-  //     'Access-Control-Allow-Origin': '*',
-  //     'content-type':'application/json',
-  //   },
-  //   withCredentials: true,
-  //   credentials: 'same-origin',
-  // });
-  // console.log(response);
-  axios({
-    method: 'get',
-    url: PROXY_URL+URL,
-  }).then(response => {
-    console.log(response);
-  })
+async function elasticsearch() {
+  const response = await axios.get('http://localhost:9200/');
+  console.log(response);
 }
 
 class App extends React.Component{
@@ -31,9 +15,6 @@ class App extends React.Component{
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {/* <p>
-            {elasticsearch()}
-          </p> */}
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
