9ca22108dc41e3b5e9ef08bd32f3264e28aaec4c
Kristianto <liauwkristianto@gmail.com>
2019-11-28 16:34:00 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function elasticsearch() {
+  const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+  const URL = 'http://localhost:9200/';
+  // const response = await axios.get('http://localhost:9200/',{
+  //   mode: 'no-cors',
+  //   headers: {
+  //     'Access-Control-Allow-Origin': '*',
+  //     'content-type':'application/json',
+  //   },
+  //   withCredentials: true,
+  //   credentials: 'same-origin',
+  // });
+  // console.log(response);
+  axios({
+    method: 'get',
+    url: PROXY_URL+URL,
+  }).then(response => {
+    console.log(response);
+  })
 }
+
+class App extends React.Component{
+  render() {
+    elasticsearch();
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <p>
+            {elasticsearch()}
+          </p> */}
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
 
 export default App;
