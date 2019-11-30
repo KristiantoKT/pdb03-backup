18a542d74b35f51fde712b020bf765c6ea9c4379
Kristianto <liauwkristianto@gmail.com>
2019-11-30 13:44:31 +0700

    Change index and use _doc to change deprecated type support

--- src/insertData.js
+++ src/insertData.js
@@ -1,6 +1,23 @@
 const axios = require('axios');
 const fs = require('fs');
 
+let create_index = async () => {
+    const URL = 'http://localhost:9200/crash'
+    const body = {
+        "settings": {
+            "number_of_shards": 3,
+            "number_of_replicas": 1
+        }
+    }
+    await axios.put(URL, body)
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
 let read_data = () => {
     let raw = fs.readFileSync('data/crashes.json');
     let data = JSON.parse(raw);
@@ -8,10 +25,9 @@ let read_data = () => {
 }
 
 let post_data = async (data) => {
-    URL = 'http://localhost:9200/nypd/crash/'
-    console.log(data.length)
+    const URL = 'http://localhost:9200/crash/_doc/'    
     for(let i = 0; i < data.length; i++) {
-        data_i = data[i];
+        let data_i = data[i];
         await axios.post(URL + i, data_i)
         .catch((error) => {
             console.log(error);
@@ -22,4 +38,5 @@ let post_data = async (data) => {
 }
 
 let data = read_data()
+create_index()
 post_data(data)
